ad2f3809ece263d67adfc960fe88b28612134b7d
Yurii Berezhnyi <yuriy.b@mobian.studio>
2021-07-22 10:47:51 +0300

    constants network module

--- app/src/main/java/com/example/marvel/data/network/Api.kt
+++ app/src/main/java/com/example/marvel/data/network/Api.kt
@@ -1,6 +1,9 @@
 package com.example.marvel.data.network
 
 import com.example.marvel.data.network.model.CharacterList
+import com.example.marvel.utils.API_KEY
+import com.example.marvel.utils.HASH
+import com.example.marvel.utils.TS
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,24 +11,24 @@ interface Api {
 
     @GET("/v1/public/characters")
     suspend fun getCharacter(
-        @Query("apikey") apiKey: String? = "8d29d02df25c8ca489092117aaee2454",
-        @Query("hash") hash: String? = "af1da214cbea559d1ddb61f64ff39129",
-        @Query("ts") ts: Int? = 1
+        @Query("apikey") apiKey: String? = API_KEY,
+        @Query("hash") hash: String? = HASH,
+        @Query("ts") ts: Int? = TS
     ): CharacterList
 
     @GET("/v1/public/characters")
     suspend fun searchCharacter(
-        @Query("apikey") apiKey: String? = "8d29d02df25c8ca489092117aaee2454",
-        @Query("hash") hash: String? = "af1da214cbea559d1ddb61f64ff39129",
-        @Query("ts") ts: Int? = 1,
+        @Query("apikey") apiKey: String? = API_KEY,
+        @Query("hash") hash: String? = HASH,
+        @Query("ts") ts: Int? = TS,
         @Query("name") name: String
     )  : CharacterList
 
     @GET("/v1/public/comics")
     suspend fun getComics(
-        @Query("apikey") apiKey: String? = "8d29d02df25c8ca489092117aaee2454",
-        @Query("hash") hash: String? = "af1da214cbea559d1ddb61f64ff39129",
-        @Query("ts") ts: Int? = 1
+        @Query("apikey") apiKey: String? = API_KEY,
+        @Query("hash") hash: String? = HASH,
+        @Query("ts") ts: Int? = TS
     ): CharacterList
 
 }
---
+++ app/src/main/java/com/example/marvel/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.marvel.utils
+
+const val API_KEY = "8d29d02df25c8ca489092117aaee2454"
+
+const val HASH = "af1da214cbea559d1ddb61f64ff39129"
+
+const val TS = 1
